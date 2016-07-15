40d33af85d10b34330a78a7f9007619c1ad3fe84
Go Takahana <tosyogontree@gmail.com>
2016-07-15 23:26:22 +0900

    Icon was updated.

--- TenkaiApp/TenkaiApp/app/build/generated/source/r/debug/com/hatsumei/tenkaiapp/R.java
+++ TenkaiApp/TenkaiApp/app/build/generated/source/r/debug/com/hatsumei/tenkaiapp/R.java
@@ -70,6 +70,7 @@ public final class R {
     }
     public static final class mipmap {
         public static final int ic_launcher=0x7f020000;
+        public static final int screenshot_2016_7_15=0x7f020001;
     }
     public static final class raw {
         public static final int hangouts_video_call=0x7f040000;
